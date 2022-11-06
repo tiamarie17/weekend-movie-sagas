396db331572da217c2241b7bfb0939c7dc484560
Tia Theisen <tiatheisen@gmail.com>
2022-11-06 13:27:11 -0600

    edit onClick function for image buttons, movie id is now defined

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
 import {useHistory} from 'react-router-dom';
@@ -9,18 +9,22 @@ function MovieList() {
     const dispatch = useDispatch();
     const history = useHistory();
 
+    //getting movies from redux store
     const movies = useSelector(store => store.movies);
+
 
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
 
     //image click handler function
-    const handlePosterClick = (event) => {
-        let imageId = event.currentTarget.id;
-        console.log('in handlePosterClick, imageId is', imageId );
+    const handlePosterClick = (movie) => {
+        console.log('movie is', movie);
+        console.log('in handlePosterClick');
+        console.log('movieId is', movie.id);
 
-        history.push(`/details/${imageId}}`);
+
+        history.push(`/details/${movie.id}}`);
 
     }
 
@@ -32,7 +36,8 @@ function MovieList() {
                     return (
                         <div key={movie.id} >
                             <h3>{movie.title}</h3>
-                            <img onClick={handlePosterClick} src={movie.poster} alt={movie.title}/>
+                            <img onClick= {handlePosterClick.bind(this, movie)} src={movie.poster} alt={movie.title}/>
+                            {/* Found .bind method on Stack Overflow: https://stackoverflow.com/questions/43024593/passing-data-up-in-an-onclick-function-in-react */}
                         </div>
         
                         
